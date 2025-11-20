c6a69e2d9a2efcee20f2347b7b576af49467d16a
TimurKradetskyi <timurkradetskyi@gmail.com>
2025-11-20 11:22:15 +0100

    dodanie początku i końca

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,47 +1,62 @@
-import {useRef, useState, useEffect } from 'react'
+import { useRef, useEffect } from 'react'
 import './App.css'
+import dijkstraImg from "./assets/dijkstra.png";
+import filippaImg from "./assets/filippa.png";
+
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const canvasHeight = 500;
   const canvasWidth = 500;
 
-  function drawBoard(){
+  function drawBoard(img1: HTMLImageElement, img2: HTMLImageElement) {
     const canvas = canvasRef.current;
     if (!canvas) return;
- 
+
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    ctx.clearRect(0, 0, canvasWidth,canvasHeight);
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     ctx.strokeStyle = "black";
     ctx.lineWidth = 1;
-    var x=0;
-    var y = 0;
-    for(let i=0;i<10;i++){
-      for(let j=0;j<10;j++){
-        ctx.save();
-        y=i*50
-        x=j*50
-        ctx.strokeRect(x, y, 50, 50);
-        ctx.restore();
+
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const x = j * 50;
+        const y = i * 50;
+
+        if (i === 1 && j === 3) {
+          ctx.drawImage(img1, x, y, 50, 50);
+        }
+        else if(i===8 && j===7){
+          ctx.drawImage(img2, x, y, 50, 50);
+        } else {
+          ctx.strokeRect(x, y, 50, 50);
+        }
       }
     }
-
   }
+
   useEffect(() => {
-    drawBoard();
+    const img1 = new Image();
+    img1.src = dijkstraImg;
+    const img2 = new Image();
+    img2.src = filippaImg;
+
+    img2.onload = () => {
+      drawBoard(img1, img2);
+    };
   }, []);
 
   return (
     <>
-    <canvas
+      <canvas
         ref={canvasRef}
         width={canvasWidth}
         height={canvasHeight}
-        style={{ border: "1px solid #000000ff"}}
+        style={{ border: "1px solid #000000ff" }}
       />
     </>
   )
 }
 
-export default App
+export default App;
